fcc11fecb7c01e7aaa7c1d09a9290e34f2b89e26
Derek Brown <djb3718@rit.edu>
2014-10-09 20:18:56 -0400

    Created java file for algorithm implementation

---
+++ CSCI261_hw3_p3/src/DivKSubsequence.java
@@ -0,0 +1,16 @@
+/*
+ * DivKSubsequence.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ * 
+ * purpose	Algorithm for finding the longest subsequence of numbers where
+ * 			no two consecutive elements have the same remainder when divided
+ * 			by k
+ */
+
+public class DivKSubsequence {
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end DivKSubsequence
